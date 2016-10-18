2d74f30cd2eb88891bf8a66c602861b64b6bac60
andreacent <andreacent8@gmail.com>
2016-10-17 21:11:24 -0400

    Readme

---
+++ act_3/README.md
@@ -0,0 +1,4 @@
+#Example <br />
+
+> make pancake16.act3 <br />
+> ./pancake16.act3 
--- algorithms/actividad3.cpp
+++ algorithms/actividad3.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <ctime>
 #include <sstream>
 #include <string>
@@ -61,15 +62,26 @@ void iddfs(state_t state){
 	printf("%d, %d, %f, %.10e\n",cost,nodes,t_final,nodes/t_final);			
 };
 
+string SplitFilename (const std::string& str){
+  size_t found = str.find_last_of(".");
+  string filename = str.substr(0,found);
+  found = filename.find_last_of("/");
+  return filename.substr(found+1);
+}
+
 int main(int argc, char const *argv[]) {
 	state_t state;
 
+	//obtengo el nombre del archivo
+	string str = argv[0];
+	string filename = SplitFilename(str);
+
 	stringstream ss;
-	ss << "../instances/" << argv[1] <<".txt";
+	ss << "../instances/" << filename <<".txt";
 	ifstream myfile(ss.str());
 
 	printf("grupo, algorithm, domain, instance, cost, generated, time, gen_per_sec\n");
-	printf("X, dfid, %s, \"",argv[1]);
+	printf("X, dfid, %s, \"",filename.c_str());
 
 	for( string line; getline( myfile, line ); ){
     	// CONVERT THE LINE TO A STATE
