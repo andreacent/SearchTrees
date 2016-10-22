4d7d4c2315b7e0f87dafbaf8ec4d0892f11acc82
andreacent <andreacent8@gmail.com>
2016-10-21 20:59:06 -0400

    Resultados ida pancake

--- act_4/act.sh
+++ act_4/act.sh
@@ -6,16 +6,16 @@ MAXMEM_MB=2048
 
 make $1.a_est
 	
-rm $1.csv
-touch $1.csv
+rm $1_a.csv
+touch $1_a.csv
 
 echo $1
 
-echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $1.csv
+echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $1_a.csv
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	ulimit -Sv $(($MAXMEM_MB*1024))
 	echo "$line"
-	printf "$line" | timeout --signal=SIGKILL 10s ./$1.a_est $2 >> $1.csv
+	printf "$line" | timeout --signal=SIGKILL 10s ./$1.a_est $2 >> $1_a.csv
 	ulimit -Sv unlimited
 done < "../instances/$1.txt"
---
+++ act_4/ida.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1,pmem=2GB,walltime=01:00:00
+#PBS -m n
+
+# $1 nombre de la instancia
+
+MAXMEM_MB=2048
+
+make $1.ida_g
+	
+rm $1_ida.csv
+touch $1_ida.csv
+
+echo $1
+
+echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $1_ida.csv
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	ulimit -Sv $(($MAXMEM_MB*1024))
+	echo "$line"
+	printf "$line" | timeout --signal=SIGKILL 10s ./$1.ida_g >> $1_ida.csv
+	ulimit -Sv unlimited
+done < "../instances/$1.txt"
