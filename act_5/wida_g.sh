556ef3d92b6d773839c7aa75ec4baac3adcf2b17
andreacent <andreacent8@gmail.com>
2016-10-21 21:18:18 -0400

    resultados wida

---
+++ act_5/wida_g.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1,pmem=2GB,walltime=01:00:00
+#PBS -m n
+
+# $1 nombre de la instancia
+# $2 peso
+
+MAXMEM_MB=2048
+
+make $1.ida_g
+	
+rm $1_ida_$2.csv
+touch $1_ida_$2.csv
+
+echo $1
+
+echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> $1_ida_$2.csv
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	ulimit -Sv $(($MAXMEM_MB*1024))
+	echo "$line"
+	printf "$line" | timeout --signal=SIGKILL 10s ./$1.ida_g $2 >> $1_ida_$2.csv
+	ulimit -Sv unlimited
+done < "../instances/$1.txt"
